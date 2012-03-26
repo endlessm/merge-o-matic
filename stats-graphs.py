09ffb829b1fa9489516e2abb17037f991df05120
Alexandre Rostovtsev <alexandre.rostovtsev@collabora.com>
2012-03-26 12:01:19 -0400

    stats-graphcs: ensure $ROOT/merges exists

--- stats-graphs.py
+++ stats-graphs.py
@@ -217,6 +217,7 @@ def pie_chart(component, current):
                info_to_data(None, current))
 
     filename = "%s/merges/%s-now.png" % (ROOT, component)
+    ensure(filename)
     with closing(canvas.init(filename, format="png")) as c:
         ar = area.T(size=(300,250), legend=None,
                     x_grid_style=None, y_grid_style=None)
@@ -246,6 +247,7 @@ def range_chart(component, history, start, today, events):
                      sources_intervals(max(d[-1] for d in data))
 
     filename = "%s/merges/%s-trend.png" % (ROOT, component)
+    ensure(filename)
     with closing(canvas.init(filename, format="png")) as c:
         ar = area.T(size=(450,225), legend=legend.T(),
                     x_axis=axis.X(label="Date", format=ordinal_to_label,
