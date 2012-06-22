9c491180209729e580bd69b744212cb0c8dc0436
Alexandre Rostovtsev <alexandre.rostovtsev@collabora.com>
2012-06-22 10:21:04 -0400

    momsettings: update

--- momsettings.py
+++ momsettings.py
@@ -9,18 +9,39 @@ MOM_NAME = "DISTRO Merge-o-Matic"
 MOM_EMAIL = "mom@SERVER"
 
 # Who to send bug emails to
-RECIPIENTS = []
+RECIPIENTS = ['trever.fischer@collabora.co.uk']
 #             [ "ubuntu-archive@lists.ubuntu.com",
 #               "ubuntu-devel@lists.ubuntu.com" ]
 
 # Distribution definitions
 # For additional subprojects, use additional distro definitions
 DISTROS = {
-    "DISTRO:SUITE_A:target": {
+    "SUITE_A-target": {
         "obs": {
             "url": "https://SERVER:444",
             "project": "DISTRO:SUITE_A:target"
         },
+        "mirror": "http://SERVER:82/debian/DISTRO:/SUITE_A:/target/",
+        "dists": [ None ],
+        "components": [ None ],
+        "expire": True,
+        },
+    "SUITE_A-sdk": {
+        "obs": {
+            "url": "https://SERVER:444",
+            "project": "DISTRO:SUITE_A:sdk"
+        },
+        "mirror": "http://SERVER:82/debian/DISTRO:/SUITE_A:/sdk/",
+        "dists": [ None ],
+        "components": [ None ],
+        "expire": True,
+        },
+    "SUITE_A-devel": {
+        "obs": {
+            "url": "https://SERVER:444",
+            "project": "DISTRO:SUITE_A:development"
+        },
+        "mirror": "http://SERVER:82/debian/DISTRO:/SUITE_A:/development/",
         "dists": [ None ],
         "components": [ None ],
         "expire": True,
@@ -46,8 +67,8 @@ DISTRO_SOURCES = {
     }
 
 DISTRO_TARGETS = {
-    "DISTRO-SUITE_A-target": {
-        "distro": "DISTRO:SUITE_A:target",
+    "SUITE_A-target": {
+        "distro": "SUITE_A-target",
         "dist": None,
         "component": None,
         "sources": [ "precise+updates", ] },
