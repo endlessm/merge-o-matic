ed4f2d70c9e8d4149a00a6607d454c699b809c3f
Trever Fischer <tdfischer@fedoraproject.org>
2012-08-13 14:18:52 -0400

    Use config.targets

--- commit_merges.py
+++ commit_merges.py
@@ -32,92 +32,83 @@ def options(parser):
     parser.add_option("-d", "--dry-run", action="store_true", help="Don't actually fiddle with OBS, just print what would've happened.")
 
 def main(options, args):
-    if options.target:
-        targets = [options.target]
-    else:
-        targets = filter(lambda target: "obs" in DISTROS[DISTRO_TARGETS[target]["distro"]], DISTRO_TARGETS)
 
-    for target in targets:
-        our_distro, our_dist, our_component = get_target_distro_dist_component(target)
-        d = Distro.get(our_distro)
-        if not isinstance(d, OBSDistro):
+    for target in config.targets(args):
+      d = target.distro
+      if not isinstance(d, OBSDistro):
+        continue
+      for source in d.newestSources(target.dist, target.component):
+        try:
+          output_dir = result_dir(target, source['Package'])
+          report = read_report(output_dir)
+        except ValueError:
+          continue
+
+        package = d.package(target.dist, target.component, report['package'])
+        filepaths = report['merged_files']
+        if filepaths == []:
+            logging.warning("Empty merged file list in %s/REPORT" % output_dir)
             continue
-        for source in d.newestSources(our_dist, our_component):
-            if options.package is not None \
-                and source["Package"] not in options.package:
-                continue
-            if not PACKAGELISTS.check_target(target, None, source["Package"]):
-                continue
 
+        if target.committable:
+          logging.info("Committing changes to %s", package)
+          if not options.dry_run:
             try:
-                output_dir = result_dir(target, source["Package"])
-                report = read_report(output_dir)
-            except ValueError:
+              package.commit('Automatic update by Merge-O-Matic')
+              pass
+            except urllib2.HTTPError:
+              logging.exception("Failed to commit %s", package)
+        else:
+          logging.debug("Branching %s", package)
+          branchPkg = package.branch("home:%s:branches:%s"%(d.obsUser, d.name))
+          branch = branchPkg.distro
+          branch.sync(our_dist, our_component, [branchPkg,])
+          logging.info("Committing changes to %s, and submitting merge request to %s", branchPkg, package)
+          if report['merged_is_right']:
+            srcDistro = Distro.get(report['right_distro'])
+            for upstream in DISTRO_TARGETS[target]['sources']:
+              for src in DISTRO_SOURCES[upstream]:
+                srcDistro = Distro.get(src['distro'])
+                for component in srcDistro.components():
+                  try:
+                    pkg = srcDistro.package(src['dist'], component, package.name)
+                    pfx = pkg.poolDirectory()
+                    break
+                  except:
+                    pass
+          else:
+            pfx = result_dir(target, package.name)
+
+          for f in branchPkg.files:
+            if f.endswith(".dsc"):
+              oldDsc = '%s/%s'%(branchPkg.obsDir(), f)
+              break
+          for f in filepaths:
+            if f.endswith(".dsc"):
+              newDsc = '%s/%s'%(pfx, f)
+              break
+
+          logging.debug("Running debdiff on %s and %s", oldDsc, newDsc)
+          diff = shell.get(("debdiff", oldDsc, newDsc), okstatus=(0,1))
+          if not options.dry_run:
+            for f in branchPkg.files:
+              if f == "_link":
                 continue
-
-            package = d.package(our_dist, our_component, report['package'])
-            filepaths = report['merged_files']
-            if filepaths == []:
-                logging.warning("Empty merged file list in %s/REPORT" % output_dir)
+              try:
+                os.unlink('%s/%s'%(branchPkg.obsDir(), f))
+              except OSError:
+                pass
+            for f in filepaths:
+              if f == "_link":
                 continue
-
-            if config.get("DISTRO_TARGETS", target, "commit", default=False):
-              logging.info("Committing changes to %s", package)
-              if not options.dry_run:
-                try:
-                  #package.commit('Automatic update by Merge-O-Matic')
-                  pass
-                except urllib2.HTTPError:
-                  logging.exception("Failed to commit %s", package)
-            else:
-              logging.debug("Branching %s", package)
-              branchPkg = package.branch("home:%s:branches:%s"%(d.obsUser, d.name))
-              branch = branchPkg.distro
-              branch.sync(our_dist, our_component, [branchPkg,])
-              logging.info("Committing changes to %s, and submitting merge request to %s", branchPkg, package)
-              if report['merged_is_right']:
-                srcDistro = Distro.get(report['right_distro'])
-                for upstream in DISTRO_TARGETS[target]['sources']:
-                  for src in DISTRO_SOURCES[upstream]:
-                    srcDistro = Distro.get(src['distro'])
-                    for component in srcDistro.components():
-                      try:
-                        pkg = srcDistro.package(src['dist'], component, package.name)
-                        pfx = pkg.poolDirectory()
-                        break
-                      except:
-                        pass
-              else:
-                pfx = result_dir(target, package.name)
-
-              for f in branchPkg.files:
-                if f.endswith(".dsc"):
-                  oldDsc = '%s/%s'%(branchPkg.obsDir(), f)
-                  break
-              for f in filepaths:
-                if f.endswith(".dsc"):
-                  newDsc = '%s/%s'%(pfx, f)
-                  break
-
-              logging.debug("Running debdiff on %s and %s", oldDsc, newDsc)
-              diff = shell.get(("debdiff", oldDsc, newDsc), okstatus=(0,1))
-              for f in branchPkg.files:
-                if f == "_link":
-                  continue
-                try:
-                  os.unlink('%s/%s'%(branchPkg.obsDir(), f))
-                except OSError:
-                  pass
-              for f in filepaths:
-                if f == "_link":
-                  continue
-                shutil.copy2("%s/%s"%(pfx, f), branchPkg.obsDir())
-              if not options.dry_run:
-                try:
-                  branchPkg.commit('Automatic update by Merge-O-Matic')
-                  #branchPkg.submitMergeRequest(d.obsProject(our_dist, our_component), diff)
-                except urllib2.HTTPError:
-                  logging.exception("Failed to commit %s", branchPkg)
+              shutil.copy2("%s/%s"%(pfx, f), branchPkg.obsDir())
+            try:
+              branchPkg.commit('Automatic update by Merge-O-Matic')
+              branchPkg.submitMergeRequest(d.obsProject(our_dist, our_component), diff)
+            except urllib2.HTTPError:
+              logging.exception("Failed to commit %s", branchPkg)
+          else:
+            logging.info("Not committing, due to --dry-run")
 
 if __name__ == "__main__":
     run(main, options, usage="%prog [DISTRO...]",
