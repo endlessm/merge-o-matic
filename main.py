390242a1d5d57bac46834a95f90b696a2344a63e
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-01-14 16:30:07 +0000

    main: improve logging and error handling
    Not ignoring the exception from os.makedirs() revealed that it didn't
    actually work, because ROOT wasn't defined.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import os
 import os.path
 import shutil
@@ -43,14 +44,21 @@ def options(parser):
     parser.add_option("-t", "--target", type="string", metavar="TARGET",
                       help="Process only this distribution target")
     parser.add_option("-d", "--dry-run", action="store_true", help="Don't actually fiddle with OBS, just print what would've happened.")
-                      
+
+logger = logging.getLogger('main')
+
 def main(options, args):
-    lockdir = "%s/.lock" % config.get('ROOT')
+    logger.info('starting Merge-o-Matic')
+    logger.debug('options: %r', options)
+    logger.debug('args: %r', args)
+
+    ROOT = config.get('ROOT')
+    lockdir = "%s/.lock" % ROOT
     codedir = os.path.dirname(__file__)
-    unpackeddir = "%s/unpacked" % config.get('ROOT')
+    unpackeddir = "%s/unpacked" % ROOT
 
     # Some modules assume we're already here
-    os.chdir(config.get('ROOT'))
+    os.chdir(ROOT)
 
     # Default options values referenced in various *.main() functions
     options.exclude = None
@@ -61,15 +69,18 @@ def main(options, args):
     try:
         os.umask(002)
         try:
+            logger.debug('Locking %r', lockdir)
             os.makedirs(lockdir)
         except:
             raise Exception("LOCKED (another one running?)")
 
         try:
-            os.makedirs("%s/merges" % ROOT)
+            if not os.path.isdir('%s/merges' % ROOT):
+                os.makedirs("%s/merges" % ROOT)
             shutil.copy2("%s/addcomment.py" % codedir, "%s/merges/addcomment.py" % ROOT)
         except:
-            pass
+            logger.exception('Unable to copy addcomment.py into %s/merges:',
+                    ROOT)
 
         # Update the Sources files against new packages that have been downloaded
         update_sources.main(options, args)
@@ -78,7 +89,7 @@ def main(options, args):
             # Try to download missing base versions from the source distro pool
             get_missing_bases.main(options, args)
         except:
-            sys.excepthook(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
+            logger.exception('Failed to get missing bases:')
 
         # Generate changes, diffs and patches
         generate_diffs.main(options, args)
@@ -106,15 +117,18 @@ def main(options, args):
         # ?! untidy
         try:
             for entry in os.listdir(unpackeddir):
-                shutil.rmtree("%s/%s" % (unpackeddir, entry))
-        except:
-            pass
-        
+                p = "%s/%s" % (unpackeddir, entry)
+                logger.debug('Removing unpacked directory %s', p)
+                shutil.rmtree(p)
+        except Exception as e:
+            logger.debug('Cancelling removal of unpacked directories: %r', e)
+
     finally:
         try:
+            logger.debug('Unlocking %r', lockdir)
             os.rmdir(lockdir)
-        except:
-            pass
+        except Exception as e:
+            logger.debug('Failed to unlock %r: %r', lockdir, e)
 
 if __name__ == "__main__":
     run(main, options, usage="%prog",
