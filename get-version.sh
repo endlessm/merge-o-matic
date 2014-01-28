582cb3af8735f92cea970872365bdbb8df934c19
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-01-28 13:03:34 +0000

    get-version.sh: be compatible with older dpkg-parsechangelog

--- get-version.sh
+++ get-version.sh
@@ -5,8 +5,8 @@ if v=$(git describe --tags --dirty --long --match '201?.*' --always); then
   exit 0
 fi
 
-if v=$(dpkg-parsechangelog -SVersion); then
-  echo "$v+~unknown-changes"
+if v=$(dpkg-parsechangelog | grep '^Version: '); then
+  echo "${v#Version: }+~unknown-changes"
   exit 0
 fi
 
