af32531982d99c023c0c6761c21b249850feebaf
Daniel Drake <drake@endlessm.com>
2018-07-09 11:54:00 -0500

    Add basic produce_merge() tests

--- tests/produceMergesTests.py
+++ tests/produceMergesTests.py
@@ -2,7 +2,10 @@ import unittest
 import config
 import testhelper
 import produce_merges
+from copy import copy
+from momlib import result_dir
 from testhelper import config_add_distro_from_repo, config_add_distro_sources
+from merge_report import MergeResult
 
 class FindUpstreamTest(unittest.TestCase):
   def setUp(self):
@@ -102,3 +105,127 @@ class FindUnstableUpstreamTest(unittest.TestCase):
     self.assertEqual(upstream.package.name, 'foo')
     self.assertEqual(upstream.version, '2.1')
     self.assertTrue(upstream > pkg_version)
+
+class ProduceMergeTest(unittest.TestCase):
+  def setUp(self):
+    self.target_repo, self.source1_repo, self.source2_repo = \
+      testhelper.standard_simple_config(num_stable_sources=2)
+
+  # Target has foo-1.0
+  # Source has foo-2.0
+  # Target should be synced to new version
+  def test_simpleSync(self):
+    foo = testhelper.build_and_import_simple_package('foo', '1.0',
+                                                     self.target_repo)
+
+    foo.changelog_entry('2.0')
+    foo.build()
+    self.source1_repo.importPackage(foo)
+
+    target = config.targets()[0]
+    testhelper.update_all_distro_sources()
+    testhelper.update_all_distro_source_pools()
+
+    our_version = target.distro.findPackage(foo.name, version='1.0')[0]
+    upstream = target.findSourcePackage(foo.name, '2.0')[0]
+
+    output_dir = result_dir(target.name, foo.name)
+    report = produce_merges.produce_merge(target, our_version, upstream,
+                                          output_dir)
+    self.assertEqual(report.result, MergeResult.SYNC_THEIRS)
+    self.assertEqual(report.merged_version, upstream.version)
+
+  # Base version foo-1.0-1
+  # Target has foo-1.0-1mom1 with modified changelog only
+  # Source has foo-1.2-1
+  # Target version should be synced to source version
+  def test_onlyDiffIsChangelog(self):
+    package = testhelper.build_and_import_simple_package('foo', '1.0-1',
+                                                         self.source1_repo)
+
+    forked = copy(package)
+    forked.changelog_entry(version='1.0-1mom1')
+    forked.build()
+    self.target_repo.importPackage(forked)
+
+    package.changelog_entry(version='1.2-1')
+    package.create_orig()
+    package.build()
+    self.source2_repo.importPackage(package)
+
+    target = config.targets()[0]
+    testhelper.update_all_distro_sources()
+    testhelper.update_all_distro_source_pools()
+
+    our_version = target.distro.findPackage(package.name, version='1.0-1mom1')[0]
+    upstream = target.findSourcePackage(package.name, version='1.2-1')[0]
+
+    output_dir = result_dir(target.name, package.name)
+    report = produce_merges.produce_merge(target, our_version, upstream,
+                                          output_dir)
+    self.assertEqual(report.result, MergeResult.SYNC_THEIRS)
+    self.assertEqual(report.merged_version, upstream.version)
+
+  # Base version foo-1.0-1
+  # Target has foo-1.0-1mom1 with a new file
+  # Source has foo-1.2-1
+  # Target version should be merged with source version
+  def test_mergeNewFile(self):
+    package = testhelper.build_and_import_simple_package('foo', '1.0-1',
+                                                         self.source1_repo)
+
+    forked = copy(package)
+    forked.changelog_entry(version='1.0-1mom1')
+    open(forked.pkg_path + '/debian/new.file', 'w').write('hello')
+    forked.build()
+    self.target_repo.importPackage(forked)
+
+    package.changelog_entry(version='1.2-1')
+    package.create_orig()
+    package.build()
+    self.source2_repo.importPackage(package)
+
+    target = config.targets()[0]
+    testhelper.update_all_distro_sources()
+    testhelper.update_all_distro_source_pools()
+
+    our_version = target.distro.findPackage(package.name, version='1.0-1mom1')[0]
+    upstream = target.findSourcePackage(package.name, version='1.2-1')[0]
+
+    output_dir = result_dir(target.name, package.name)
+    report = produce_merges.produce_merge(target, our_version, upstream,
+                                          output_dir)
+    self.assertEqual(report.result, MergeResult.MERGED)
+    self.assertTrue(report.merged_version > upstream.version)
+
+  # Base version foo-1.0-1
+  # Target has foo-1.0-1mom1 with a new file
+  # Source has foo-1.2-1 with a conflicting new file
+  # Merge should fail due to conflicts
+  def test_mergeConflicts(self):
+    package = testhelper.build_and_import_simple_package('foo', '1.0-1',
+                                                         self.source1_repo)
+
+    forked = copy(package)
+    forked.changelog_entry(version='1.0-1mom1')
+    open(forked.pkg_path + '/debian/new.file', 'w').write('hello')
+    forked.build()
+    self.target_repo.importPackage(forked)
+
+    package.changelog_entry(version='1.2-1')
+    open(package.pkg_path + '/debian/new.file', 'w').write('conflict')
+    package.create_orig()
+    package.build()
+    self.source2_repo.importPackage(package)
+
+    target = config.targets()[0]
+    testhelper.update_all_distro_sources()
+    testhelper.update_all_distro_source_pools()
+
+    our_version = target.distro.findPackage(package.name, version='1.0-1mom1')[0]
+    upstream = target.findSourcePackage(package.name, version='1.2-1')[0]
+
+    output_dir = result_dir(target.name, package.name)
+    report = produce_merges.produce_merge(target, our_version, upstream,
+                                          output_dir)
+    self.assertEqual(report.result, MergeResult.CONFLICTS)
