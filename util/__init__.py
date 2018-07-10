d672fda77dc9aa9b49f2e230a34b70cee615f2df
Daniel Drake <drake@endlessm.com>
2018-07-10 15:16:10 -0500

    util: move option parser creation to separate method
    This is needed for unit testing of the whole app.

--- util/__init__.py
+++ util/__init__.py
@@ -33,13 +33,7 @@ def pathhash(path):
 # --------------------------------------------------------------------------- #
 # Command-line tool functions
 # --------------------------------------------------------------------------- #
-
-def run(main_func, options_func=None, usage=None, description=None):
-    """Run the given main function after initialising options."""
-    logging.Formatter.converter = time.gmtime
-    logging.basicConfig(format="%(asctime)s  %(name)s.%(levelname)s: %(message)s", datefmt="%a, %d %b %Y %H:%M:%S +0000") # RFC 2822
-    logging.getLogger().setLevel(logging.INFO)
-
+def get_option_parser(usage=None, description=None):
     parser = OptionParser(usage=usage, description=description)
     parser.add_option("-q", "--quiet", action="callback",
                       callback=quiet_callback, help="Be less chatty")
@@ -47,6 +41,16 @@ def run(main_func, options_func=None, usage=None, description=None):
                       callback=verbose_callback, help="Be more noisy")
     parser.add_option("-p", "--package", type="string", metavar="PACKAGE", action="append",
                       help="Process only this package")
+    return parser
+
+
+def run(main_func, options_func=None, usage=None, description=None):
+    """Run the given main function after initialising options."""
+    logging.Formatter.converter = time.gmtime
+    logging.basicConfig(format="%(asctime)s  %(name)s.%(levelname)s: %(message)s", datefmt="%a, %d %b %Y %H:%M:%S +0000") # RFC 2822
+    logging.getLogger().setLevel(logging.INFO)
+
+    parser = get_option_parser(usage, description)
     if options_func is not None:
         options_func(parser)
 
