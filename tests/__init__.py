5102a8689aad10fa8f5f4d6d3570fbeaf0ab7070
Daniel Drake <drake@endlessm.com>
2018-07-17 12:08:15 -0500

    get_missing_bases: access snapshot.debian.org rather than using debsnap
    In preparation for reworking behaviour in this area, rewrite
    get_missing_bases to use direct access to snapshot.debian.org rather
    than calling out to debsnap.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,11 @@
 import os
 import logging
 import shutil
+import tempfile
+import shutil
+import atexit
+
+import get_missing_bases
 
 os.environ['MOM_TEST'] = '1'
 
@@ -10,3 +15,8 @@ if 'MOM_TEST_DEBUG' in os.environ:
     logging.basicConfig(level=logging.DEBUG)
 else:
     logging.basicConfig(level=logging.INFO)
+
+debsnap_base = tempfile.mkdtemp(prefix='momtest.debsnap.')
+get_missing_bases.BASE_URL = 'file://' + debsnap_base
+if 'MOM_TEST_NO_CLEANUP' not in os.environ:
+  atexit.register(shutil.rmtree, debsnap_base)
